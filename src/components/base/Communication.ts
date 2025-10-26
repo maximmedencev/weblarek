d11a587c6500711737fbbe4d93a7c05d1126fe2d
Maxim Medencev <maxim.medencev@yandex.ru>
2025-10-26 15:40:31 +0700

    Исправить название приватного поля в классе DataExchanger

--- src/components/base/Communication.ts
+++ src/components/base/Communication.ts
@@ -1,17 +1,17 @@
 import { ICatalogData, IOrder, IApi, IOrderResponse } from "../../types";
 
 export class DataExchanger {
-  private _api: IApi;
+  private api: IApi;
 
   constructor(api: IApi) {
-    this._api = api;
+    this.api = api;
   }
 
   async getProducts(): Promise<ICatalogData> {
-    return await this._api.get("/product/");
+    return await this.api.get("/product/");
   }
 
   async sendOrder(orderData: IOrder): Promise<IOrderResponse> {
-    return await this._api.post("/order/", orderData);
+    return await this.api.post("/order/", orderData);
   }
 }
